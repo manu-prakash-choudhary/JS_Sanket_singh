a8e74c58514bd874de49d1eba86b9c3db59745d7
manuprakash <manu.prakash2020@vitbhopal.ac.in>
2023-06-09 00:16:53 +0530

    Sanket Singh Repor First Commit

---
+++ globalsInNode.js
@@ -0,0 +1,3 @@
+console.log(process);
+console.log(__dirname);         // It will return the current working directory
+                                // in some cases it is not valid 
---
+++ hello.js
@@ -0,0 +1,12 @@
+function fetchdata(data) {
+    console.log(data, 'fetching started')
+    return new Promise((resolve, reject) => {
+        resolve(`data fetch succesfully ${data}`)
+    })
+    
+}
+let x = fetchdata("asdfdf")
+x.then((value => {
+    console.log(value)
+}))
+
