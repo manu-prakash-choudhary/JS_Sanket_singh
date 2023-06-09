79b6ea758a6cd729b23c60cb1821368da79c4bbc
manuprakash <manu.prakash2020@vitbhopal.ac.in>
2023-06-09 21:42:22 +0530

    fd

--- 09_JUNE_PROJECT/index.js
+++ 09_JUNE_PROJECT/index.js
@@ -2,3 +2,9 @@
 // BotLink : http://t.me/coding_manu_bot
 // BotUserName : coding_manu_bot
 // BotName : coding_bot_manu
+
+// 1. Import telegraf
+const { Telegraf } = require('telegraf');
+
+// 2. Create a bot using the token provided by BotFather
+
