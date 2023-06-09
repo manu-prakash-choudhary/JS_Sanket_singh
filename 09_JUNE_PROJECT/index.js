f22dd58bf0221e06a8bd842336af1b8f3cebad87
manuprakash <manu.prakash2020@vitbhopal.ac.in>
2023-06-09 21:50:16 +0530

    sdf

--- 09_JUNE_PROJECT/index.js
+++ 09_JUNE_PROJECT/index.js
@@ -7,4 +7,14 @@
 const { Telegraf } = require('telegraf');
 
 // 2. Create a bot using the token provided by BotFather
+// const Bot = new Telegraf('YOUR_TOKEN');
+const bot = new Telegraf('5705838954:AAGJNoNY8Xl8Ok9skMA4kTMZk5Jehr6oeLY');
+
+
+
+bot.start((ctx) => { ctx.reply('Chl na be laude') });
+bot.help((ctx) => { ctx.reply('10 rupaye doge toh ek raaj ki baat btaunga') });
+
+// 3. Start the bot using bot.launch() method
+bot.launch();
 
